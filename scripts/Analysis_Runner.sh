93df7332bd53c63443fbbcbc583e1a5b9460c9c9
Adam Faranda <adam.faranda@gmail.com>
2021-12-01 12:54:08 -0500

    Fixed indexing for chunked imputation

--- scripts/Analysis_Runner.sh
+++ scripts/Analysis_Runner.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #SBATCH --mem=128000
 
+# Create result directories
+mkdir -p analysis_objects
+mkdir -p results
+
 ## Run Exploration and Preprocessing
 jupyter nbconvert --to pdf\
     --output-dir=results\
