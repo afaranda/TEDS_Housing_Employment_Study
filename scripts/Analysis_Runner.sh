af373e82c3aeed9429dd41f81eb4239879c6fb44
Adam Faranda <adam.faranda@gmail.com>
2021-12-01 21:10:19 -0500

    added environment activation

--- scripts/Analysis_Runner.sh
+++ scripts/Analysis_Runner.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #SBATCH --mem=128000
 
+
 # Create result directories
 mkdir -p analysis_objects
 mkdir -p results
+
+# Activate Conda Environment
+conda activate binf667013_final_project
 
 ## Run Exploration and Preprocessing
 jupyter nbconvert --to pdf\
