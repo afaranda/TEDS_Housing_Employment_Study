6977efa9609f55e6d2c992afcab9467b7b0032e7
Adam Faranda <abf@udel.edu>
2021-12-09 14:08:44 -0500

    Updates and Bug Fixes

--- scripts/Analysis_Runner.sh
+++ scripts/Analysis_Runner.sh
@@ -7,6 +7,13 @@ mkdir -p analysis_objects
 mkdir -p results
 
 ## Run Exploration and Preprocessing
-jupyter nbconvert --to pdf\
+jupyter nbconvert --to html\
     --output-dir=results\
     --execute notebooks/01_Explore_And_Process.ipynb
+
+## Run Logistic Models for IC Laws on Target Endpoints
+jupyter nbconvert --to html\
+    --output-dir=results\
+    --execute notebooks/02_Logistic_Models.ipynb
+
+
