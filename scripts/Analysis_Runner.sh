0af9b29df3db961c7a590ccfa5649b876a6d1140
Adam Faranda <adam.faranda@gmail.com>
2021-12-14 10:25:16 -0500

    Keep on truckin

--- scripts/Analysis_Runner.sh
+++ scripts/Analysis_Runner.sh
@@ -8,12 +8,12 @@ mkdir -p results
 
 
 ## Run Exploration and Preprocessing
-if [ -f "results/01_Explore_And_Process.html" ]
+if [ -f "results/01_Analyze_Missing_and_Impute.html" ]
 then
-    echo Found Result File: "results/01_Explore_And_Process.html"
+    echo Found Result File: "results/01_Analyze_Missing_and_Impute.html"
 else
-    echo Generating "results/01_Explore_And_Process.html"
-    sbatch scripts/run_jupyter.sh 01_Explore_And_Process.ipynb
+    echo Generating "results/01_Analyze_Missing_and_Impute.html"
+    sbatch scripts/run_jupyter.sh 01_Analyze_Missing_and_Impute.ipynb
 fi    
 
 ## Run Logistic Models for IC Laws on Target Endpoints
@@ -32,14 +32,54 @@ else
 fi
 
 ## Run Gradient Boosted Forests to predict Relapse
-if [ -f "results/03_Gradient_Boosted_Relapse_Classification.html" ]
+if [ -f "results/03_Gradient_Boosted_Relapse_Classification_All.html" ]
 then
-   echo Found Result File: "results/03_Gradient_Boosted_Relapse_Classification.html"
+   echo Found Result File: "results/03_Gradient_Boosted_Relapse_Classification_All.html"
 else
-    echo Generating "results/03_Gradient_Boosted_Relapse_Classification.html"
+    echo Generating "results/03_Gradient_Boosted_Relapse_Classification_All.html"
     if [ -f "analysis_objects/teds_imp_laws.csv" ]
     then
-	    sbatch scripts/run_jupyter.sh 03_Gradient_Boosted_Relapse_Classification.ipynb
+	    sbatch scripts/run_jupyter.sh 03_Gradient_Boosted_Relapse_Classification_All.ipynb
+    else
+	    echo Must run Notebook 01_Explore_And_Processs.ipynb first
+    fi
+fi
+
+
+if [ -f "results/04_Gradient_Boosted_Relapse_Classification_NoState.html" ]
+then
+   echo Found Result File: "results/04_Gradient_Boosted_Relapse_Classification_NoState.html"
+else
+    echo Generating "results/04_Gradient_Boosted_Relapse_Classification_NoState.html"
+    if [ -f "analysis_objects/teds_imp_laws.csv" ]
+    then
+	    sbatch scripts/run_jupyter.sh 04_Gradient_Boosted_Relapse_Classification_NoState.ipynb
+    else
+	    echo Must run Notebook 01_Explore_And_Processs.ipynb first
+    fi
+fi
+
+if [ -f "results/05_Gradient_Boosted_Relapse_Classification_NoIC.html" ]
+then
+   echo Found Result File: "results/05_Gradient_Boosted_Relapse_Classification_NoIC.html"
+else
+    echo Generating "results/05_Gradient_Boosted_Relapse_Classification_NoIC.html"
+    if [ -f "analysis_objects/teds_imp_laws.csv" ]
+    then
+	    sbatch scripts/run_jupyter.sh 05_Gradient_Boosted_Relapse_Classification_NoIC.ipynb
+    else
+	    echo Must run Notebook 01_Explore_And_Processs.ipynb first
+    fi
+fi
+
+if [ -f "results/06_Gradient_Boosted_Relapse_Classification_NoGeo.html" ]
+then
+   echo Found Result File: "results/06_Gradient_Boosted_Relapse_Classification_NoGeo.html"
+else
+    echo Generating "results/06_Gradient_Boosted_Relapse_Classification_NoGeo.html"
+    if [ -f "analysis_objects/teds_imp_laws.csv" ]
+    then
+	    sbatch scripts/run_jupyter.sh 06_Gradient_Boosted_Relapse_Classification_NoGeo.ipynb
     else
 	    echo Must run Notebook 01_Explore_And_Processs.ipynb first
     fi
